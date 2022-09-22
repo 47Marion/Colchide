fb1ed7d7136389ba75f6e8538da09b8416b575cf
47Marion <113423316+47Marion@users.noreply.github.com>
2022-09-22 10:18:43 +0200

    modif script

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const message = "En vous remerciant de votre humble message";
-
-
-
 document.getElementById("contactForm")
 .addEventListener("Submit", function (event) {
     event.preventDefault();
